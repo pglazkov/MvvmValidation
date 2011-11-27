d0b22ffe1297678973a399800e76b2e5e6be5f44
pglazkov <p.glazkov@gmail.com>
2011-11-27 12:54:21 +0100

    Create a ValidatableViewModelBase.

---
+++ Examples/FormValidationExample.Silverlight/ValidatableViewModelBase.cs
@@ -0,0 +1,27 @@
+﻿using GalaSoft.MvvmLight;
+using MvvmValidation;
+
+namespace FormValidationExample
+{
+	public partial class ValidatableViewModelBase : ViewModelBase
+	{
+		protected ValidationHelper Validator { get; private set; }
+
+#if SILVERLIGHT 
+		private NotifyDataErrorInfoAdapter NotifyDataErrorInfoAdapter { get; set; }
+#else
+		private DataErrorInfoAdapter DataErrorInfoAdapter { get; set; }
+#endif
+
+		public ValidatableViewModelBase()
+		{
+			Validator = new ValidationHelper();
+
+#if SILVERLIGHT
+			NotifyDataErrorInfoAdapter = new NotifyDataErrorInfoAdapter(Validator);
+#else
+			DataErrorInfoAdapter = new DataErrorInfoAdapter(Validator);
+#endif
+		}
+	}
+}
---
+++ Examples/FormValidationExample/ValidatableViewModelBase.Desktop.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+
+namespace FormValidationExample
+{
+	public partial class ValidatableViewModelBase : IDataErrorInfo
+	{
+		public string this[string columnName]
+		{
+			get { return DataErrorInfoAdapter[columnName]; }
+		}
+
+		public string Error
+		{
+			get { return DataErrorInfoAdapter.Error; }
+		}
+	}
+}
