0fee3f761e6cb27e5be7390290dc92e6f55320a1
pglazkov <p.glazkov@gmail.com>
2011-12-01 08:26:51 +0100

    A little reorganization of the example rules.

--- Examples/FormValidationExample.Silverlight/ViewModel/MainViewModel.cs
+++ Examples/FormValidationExample.Silverlight/ViewModel/MainViewModel.cs
@@ -113,41 +113,40 @@ namespace FormValidationExample
 		{
 			Validator.AddRule(() => FirstName,
 			                  () => RuleResult.Assert(!string.IsNullOrEmpty(FirstName), "First Name is required"));
+
 			Validator.AddRule(() => LastName,
 			                  () => RuleResult.Assert(!string.IsNullOrEmpty(LastName), "Last Name is required"));
+
 			Validator.AddRule(() => Email,
 			                  () => RuleResult.Assert(!string.IsNullOrEmpty(Email), "Email is required"));
+
 			Validator.AddRule(() => Email,
 			                  () =>
 			                  {
-			                  	if (string.IsNullOrEmpty(Email))
-			                  	{
-			                  		return RuleResult.Valid();
-			                  	}
-
 			                  	const string regexPattern =
 			                  		@"^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$";
-			                  	return RuleResult.Assert(Regex.IsMatch(Email, regexPattern), "Email must by a valid email address");
+			                  	return RuleResult.Assert(string.IsNullOrEmpty(Email) || Regex.IsMatch(Email, regexPattern),
+			                  	                         "Email must by a valid email address");
 			                  });
+
 			Validator.AddRule(() => Password,
-			                  () =>
-			                  {
-			                  	RuleResult result = RuleResult.Assert(!string.IsNullOrEmpty(Password), "Password is required");
+			                  () => RuleResult.Assert(!string.IsNullOrEmpty(Password),
+			                                          "Password is required"));
 
-			                  	if (result.IsValid)
-			                  	{
-			                  		Debug.Assert(Password != null);
+			Validator.AddRule(() => Password,
+			                  () => RuleResult.Assert(string.IsNullOrEmpty(Password) || Password.Length >= 6,
+			                                          "Password must contain at least 6 characters"));
 
-			                  		result = RuleResult.Assert(Password.Length >= 6, "Password must contain at least 6 characters").
-			                  			Combine(
-			                  				RuleResult.Assert(
-			                  					!Password.All(Char.IsLower) && !Password.All(Char.IsUpper) && !Password.All(Char.IsDigit),
-			                  					"Password must contain both lower case and upper case letters")).Combine(
-			                  						RuleResult.Assert(Password.Any(Char.IsDigit), "Password must contain at least one digit"));
-			                  	}
+			Validator.AddRule(() => Password,
+			                  () => RuleResult.Assert(string.IsNullOrEmpty(Password) || (!Password.All(Char.IsLower) &&
+			                                                                             !Password.All(Char.IsUpper) &&
+			                                                                             !Password.All(Char.IsDigit)),
+			                                          "Password must contain both lower case and upper case letters"));
 
-			                  	return result;
-			                  });
+			Validator.AddRule(() => Password,
+			                  () => RuleResult.Assert(string.IsNullOrEmpty(Password) || Password.Any(Char.IsDigit),
+			                                          "Password must contain at least one digit"));
+
 			Validator.AddRule(() => PasswordConfirmation,
 			                  () =>
 			                  {
@@ -158,6 +157,7 @@ namespace FormValidationExample
 
 			                  	return RuleResult.Valid();
 			                  });
+
 			Validator.AddRule(() => Password,
 			                  () => PasswordConfirmation,
 			                  () =>
