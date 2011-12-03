086f237ca59617878e9b46bad9e4455fd46aae65
pglazkov <p.glazkov@gmail.com>
2011-12-03 12:53:44 +0100

    Created a simply msbuild script and a go.cmd that builds the solution. Later will add execution of unit tests and creation of the nuget package.

---
+++ go.cmd
@@ -0,0 +1,5 @@
+set script=.\build.msbuild
+set msbuild=%windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe
+set targets=%*
+if /i "%targets%" == "" set targets=BuildFull
+%msbuild% %script% /target:%targets%
