4149b585ec8b8e076a3f05b4603afc739f2c7592
pglazkov <p.glazkov@gmail.com>
2011-12-04 11:33:57 +0100

    Getting rid of the dependency on the Reactive Extensions in the main libs.

--- MvvmValidation/ValidationErrorCollection.cs
+++ MvvmValidation/ValidationErrorCollection.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Globalization;
-using System.Linq;
-using System.Text;
 
 namespace MvvmValidation
 {
@@ -19,34 +16,6 @@ namespace MvvmValidation
 		internal ValidationErrorCollection(IList<ValidationError> list)
 			: base(list)
 		{
-		}
-
-		/// <summary>
-		/// Returns a <see cref="System.String"/> that represents this instance.
-		/// </summary>
-		/// <returns>
-		/// A <see cref="System.String"/> that represents this instance.
-		/// </returns>
-		public override string ToString()
-		{
-			ValidationError[] distinctErrors = this.Distinct(e => e.ErrorText).ToArray();
-
-			if (distinctErrors.Length == 1)
-			{
-				return this[0].ToString();
-			}
-
-			var result = new StringBuilder();
-			int counter = 1;
-
-			foreach (ValidationError error in distinctErrors)
-			{
-				result.AppendFormat(CultureInfo.InvariantCulture, "{0}. {1}", counter, error);
-				result.AppendLine();
-				counter++;
-			}
-
-			return result.ToString().Trim();
 		}
 	}
 }
