0a1bec5b0031fb526b34467e74e9f8ce210b4c5f
Pavlo <Pavlo@HOME-PC>
2013-03-10 16:49:30 +0100

    Fixed a couple of issues with NuGet package. Added nuget-publish.ps1 - a PowerShall script that helps to automate package publishing.

--- VersionInfo.cs
+++ VersionInfo.cs
@@ -16,6 +16,6 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("1.1.0")]
 [assembly: AssemblyFileVersion("1.1.0")]
-[assembly: AssemblyInformationalVersion("1.1.0-beta")]
+[assembly: AssemblyInformationalVersion("1.1.0-beta2")]
 
 
