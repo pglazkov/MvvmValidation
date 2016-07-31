780173ba53c07145e6356bb08274040596a1500d
Pavlo Glazkov <p.glazkov@gmail.com>
2016-07-31 15:44:55 +0200

    Updated version.

--- VersionInfo.cs
+++ VersionInfo.cs
@@ -16,6 +16,6 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("3.0.0")]
 [assembly: AssemblyFileVersion("3.0.0")]
-[assembly: AssemblyInformationalVersion("3.0.0-beta1")]
+[assembly: AssemblyInformationalVersion("3.0.0-beta2")]
 
 
