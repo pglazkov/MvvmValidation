00e70ff689b061d3e3dc100008e8e20023b9fedc
Pavlo Glazkov <p.glazkov@gmail.com>
2016-10-20 08:53:51 +0200

    Increased version.

--- VersionInfo.cs
+++ VersionInfo.cs
@@ -14,8 +14,8 @@ using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("3.0.0")]
-[assembly: AssemblyFileVersion("3.0.0")]
-[assembly: AssemblyInformationalVersion("3.0.0")]
+[assembly: AssemblyVersion("3.1.0")]
+[assembly: AssemblyFileVersion("3.1.0")]
+[assembly: AssemblyInformationalVersion("3.1.0")]
 
 
